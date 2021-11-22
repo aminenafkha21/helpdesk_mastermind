f62bc304af5af4bd4b3fa6b0a8bcc314261be060
amyyne-naf <aminenafkha21@gmail.com>
2021-11-22 13:35:31 +0100

    first commit

---
+++ routes/web.php
@@ -0,0 +1,65 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\TicketController;
+use App\Http\Controllers\NotificationController;
+
+
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Auth::routes([]);
+
+
+
+
+Route::middleware('auth')->group(function () {
+
+Route::get('/home', [App\Http\Controllers\HomeController::class,'index'])->name('home');
+
+Route::get('/notifications', [App\Http\Controllers\NotificationController::class,'index'])->name('notifications');
+
+
+Route::resource('tickets', TicketController::class);
+Route::get('/newticket', [App\Http\Controllers\TicketController::class, 'create']);
+
+Route::get('/{page}', [App\Http\Controllers\HomeController::class, 'error']);
+Route::get("removeticket/{id}",[TicketController::class,"removeTicket"]);
+Route::get("editticket/{id}",[TicketController::class,"showTicket"]);
+Route::post("/edit",[TicketController::class,"updateTicket"]);
+
+Route::post("/assignedticket",[TicketController::class,"assignedTicket"]);
+Route::post("/closeticket",[TicketController::class,"closeTicket"]);
+
+Route::post("/confirmticket",[TicketController::class,"confirmTicket"]);
+Route::get("/reopenticket/{id}",[TicketController::class,"reopenTicket"]);
+
+
+
+
+
+});
+
+
+Route::get('/{page}', [App\Http\Controllers\AdminController::class, 'index']);
+
+
+
